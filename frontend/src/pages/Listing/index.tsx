eafbbca27c3ec0f1626af9edc2408ad0290ee875
Thyago <t.y_josef7@hotmail.com>
2022-01-15 21:28:38 -0300

    useState,useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,15 +2,27 @@ import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Listing(){
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=1`)
-    .then(response => {
-        console.log(response.data);
-    })
+    const [pageNumber, setPageNumber] = useState(0);
 
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(response.data);
+            setPageNumber(data.number);
+        });
+    
+    }, []);
+   
+
+  
+   
     return (
 
         <>
